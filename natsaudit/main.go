ec1edc9f19e2842ea20011448062b1d6fdeef85a
sergevas <brighterr@gmail.com>
2023-04-01 15:23:30 +0300

    Improve console output

--- natsaudit/main.go
+++ natsaudit/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	nc, err := nats.Connect("nats://192.168.1.70:4222",
+	nc, err := nats.Connect("nats://192.168.1.86:4222",
 		nats.Name("natsaudit"),
-		nats.UserInfo("foo", "secret"),
+		nats.UserInfo("cg_nats_usr", "cg_nats_passwd"),
 	)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	nc.Subscribe(">", func(m *nats.Msg) {
-		log.Printf("%#v", *m)
+		log.Printf("Subject: %s\n", string(m.Subject))
+		log.Printf("Message: %s\n", string(m.Data))
+		log.Printf("--------------------")
 	})
 	runtime.Goexit()
 }
